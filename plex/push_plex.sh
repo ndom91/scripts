30523bb7ee4514c472b1e3d01c4edbdc760f6e39
ndo_nt <yo@iamnico.xyz>
2018-02-26 20:48:42 +0100

    push_plex.sh update

--- plex/push_plex.sh
+++ plex/push_plex.sh
@@ -5,14 +5,26 @@ TIME=$(date '+%d%m%Y')
 TV_DIR=/home/ndo/ftp/files/torrentcomplete/TV
 MOV_DIR=/home/ndo/ftp/files/torrentcomplete/Movies
 
+for pid in $(pidof -x push_plex.sh); do
+    if [ $pid != $$ ]; then
+        echo "[$(date)] : push_plex.sh : Process is already running with PID $pid"
+        exit 1
+    fi
+done
+
 echo "Beginning content upload to Plex."
 echo ""
 
 if [ -z "$(ls -A $TV_DIR)" ]; then
    echo "No TV Content to Upload.."
 else
+  cd $TV_DIR
+  ls -lah
+  echo ""
   echo "Uploading TV Content..."
+  echo ""
   /usr/bin/rclone move --config /home/ndo/.config/rclone/rclone.conf --log-file /home/ndo/Documents/rclone_tv_move_$TIME.log --log-level INFO $TV_DIR GdriveEnc:plex_enc/tv
+  echo ""
   echo "TV content uploaded"
   echo ""
   rm -r $TV_DIR/*
@@ -26,8 +38,13 @@ fi
 if [ -z "$(ls -A $MOV_DIR)" ]; then
    echo "No Movie Content to Upload.."
 else
+  cd $MOV_DIR
+  ls -lah
+  echo ""
   echo "Uploading Movie Content.."
+  echo ""
   /usr/bin/rclone move --config /home/ndo/.config/rclone/rclone.conf --log-file /home/ndo/Documents/rclone_movie_move_$TIME.log --log-level INFO $MOV_DIR GdriveEnc:plex_enc/movies
+  echo ""
   echo "Movie content uploaded"
   echo ""
   rm -r $MOV_DIR/*
@@ -41,3 +58,5 @@ fi
 
 
 echo "Upload script complete!"
+
+fi
