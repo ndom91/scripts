af4411e91917bb411a6d9e952297e6328c1f9af0
ndo_nt <yo@iamnico.xyz>
2018-02-26 20:57:53 +0100

    modified:   plex/push_plex.sh

--- plex/push_plex.sh
+++ plex/push_plex.sh
@@ -5,7 +5,6 @@ TIME=$(date '+%d%m%Y')
 TV_DIR=/home/ndo/ftp/files/torrentcomplete/TV
 MOV_DIR=/home/ndo/ftp/files/torrentcomplete/Movies
 
-<<<<<<< HEAD
 for pid in $(pidof -x push_plex.sh); do
     if [ $pid != $$ ]; then
         echo "[$(date)] : push_plex.sh : Process is already running with PID $pid"
@@ -13,26 +12,18 @@ for pid in $(pidof -x push_plex.sh); do
     fi
 done
 
-=======
->>>>>>> 353e0c7b6a5efb15f289082cd58f72c1fe9b34ac
 echo "Beginning content upload to Plex."
 echo ""
 
 if [ -z "$(ls -A $TV_DIR)" ]; then
    echo "No TV Content to Upload.."
 else
-<<<<<<< HEAD
   cd $TV_DIR
   ls -lah
   echo ""
   echo "Uploading TV Content..."
   echo ""
   /usr/bin/rclone move --config /home/ndo/.config/rclone/rclone.conf --log-file /home/ndo/Documents/rclone_tv_move_$TIME.log --log-level INFO $TV_DIR GdriveEnc:plex_enc/tv
-  echo ""
-=======
-  echo "Uploading TV Content..."
-  /usr/bin/rclone move --config /home/ndo/.config/rclone/rclone.conf --log-file /home/ndo/Documents/rclone_tv_move_$TIME.log --log-level INFO $TV_DIR GdriveEnc:plex_enc/tv
->>>>>>> 353e0c7b6a5efb15f289082cd58f72c1fe9b34ac
   echo "TV content uploaded"
   echo ""
   rm -r $TV_DIR/*
@@ -46,7 +37,6 @@ fi
 if [ -z "$(ls -A $MOV_DIR)" ]; then
    echo "No Movie Content to Upload.."
 else
-<<<<<<< HEAD
   cd $MOV_DIR
   ls -lah
   echo ""
@@ -54,10 +44,6 @@ else
   echo ""
   /usr/bin/rclone move --config /home/ndo/.config/rclone/rclone.conf --log-file /home/ndo/Documents/rclone_movie_move_$TIME.log --log-level INFO $MOV_DIR GdriveEnc:plex_enc/movies
   echo ""
-=======
-  echo "Uploading Movie Content.."
-  /usr/bin/rclone move --config /home/ndo/.config/rclone/rclone.conf --log-file /home/ndo/Documents/rclone_movie_move_$TIME.log --log-level INFO $MOV_DIR GdriveEnc:plex_enc/movies
->>>>>>> 353e0c7b6a5efb15f289082cd58f72c1fe9b34ac
   echo "Movie content uploaded"
   echo ""
   rm -r $MOV_DIR/*
@@ -67,12 +53,4 @@ else
   echo ""
   echo "Plex Movie Refreshed."
 fi
-
-
-
 echo "Upload script complete!"
-<<<<<<< HEAD
-
-fi
-=======
->>>>>>> 353e0c7b6a5efb15f289082cd58f72c1fe9b34ac
