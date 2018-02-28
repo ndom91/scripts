c9d9fb6635f4fda003386f8c3e38175a61a71e10
Nico Domino <yo@iamnico.xyz>
2018-02-28 08:38:13 +0100

    mount updated

--- mounts/sshfs_ndo0.sh
+++ mounts/sshfs_ndo0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/sshfs -p 1022 -o allow_other,default_permissions,uid=1000,gid=1000,IdentityFile=/home/ndo/Documents/Config/ndo0_pair0[openssh].ppk ndom@ndo0.iamnico.xyz:/ /home/ndo/Documents/ndo0_home
+/usr/bin/sshfs -p 1022 -o allow_other,default_permissions,uid=1000,gid=1000,IdentityFile=/home/ndo/Documents/Config/ndo0_pair0[openssh].ppk ndom@ndo0.iamnico.xyz:/ /opt/ndo0_home
