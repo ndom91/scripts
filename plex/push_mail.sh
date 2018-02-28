77ab432c6f495a716d8af932646cc03c2d3dc7ab
ndo_nt <yo@iamnico.xyz>
2018-02-28 19:11:19 +0100

    added jekyll layout 	new file:   _layouts/default.html 	modified:   plex/push_mail.sh

--- plex/push_mail.sh
+++ plex/push_mail.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 CURDATE=$(date '+%d-%m-%Y %H:%M')
+PLEXPATH=/home/ndo/Documents/scripts/plex
 
-/home/ndo/Documents/scripts/plex/push_plex3.sh > /home/ndo/Documents/scripts/plex/push_output.txt
-output=$(cat push_output.txt)
+/home/ndo/Documents/scripts/plex/push_plex3.sh > $PLEXPATH/push_output.txt
+output=$(cat $PLEXPATH/push_output.txt)
 
-countmoved=$(grep -c "Nothing moved - no need to refresh!" "push_output.txt")
+countmoved=$(grep -c "Nothing moved - no need to refresh!" $PLEXPATH/push_output.txt)
 
 if (( $countmoved == 2 )); then
 	exit 1
 else
-	(echo "From: ndo <ndo0@iamnico.xyz>";
+	(echo "From: ndo2 <ndo2@iamnico.xyz>";
         echo "To: yo@iamnico.xyz";
         echo "Subject: Plex upload complete - $CURDATE";
 
-        echo "$output";) | sendmail -oi yo@iamnico.xyz;
+        echo "$output";) | /usr/sbin/sendmail -oi yo@iamnico.xyz;
 
 
 fi
