1d116fdf4273955af951f537fd592b0b5e7f3353
ndom91 <yo@iamnico.xyz>
2018-03-01 16:44:25 +0100

    added music to plex_push3.sh 	modified:   plex/push_plex3.sh

--- plex/push_plex3.sh
+++ plex/push_plex3.sh
@@ -5,11 +5,15 @@ MAILTO="yo@iamnico.xyz"
 TIME=$(date '+%d%m%Y')
 TV_DIR=/home/ndo/ftp/files/torrentcomplete/tv
 MOV_DIR=/home/ndo/ftp/files/torrentcomplete/movies
+MUS_DIR=/home/ndo/ftp/files/torrentcomplete/music
 
 TV_ARRAY=(/home/ndo/ftp/files/torrentcomplete/tv/*) 
 MOV_ARRAY=(/home/ndo/ftp/files/torrentcomplete/movies/*) 
+MUS_ARRAY=(/home/ndo/ftp/files/torrentcomplete/music/*) 
+
 tv_counter=0
 mov_counter=0
+mus_counter=0
 
 for pid in $(pidof -x push_plex.sh); do
     if [ $pid != $$ ]; then
@@ -82,6 +86,34 @@ else
 	  curl http://ndo2.iamnico.xyz:32400/library/sections/5/refresh?X-Plex-Token=UpkkEa7jE1dmneA4orEm >> /dev/null 2>&1
 	  echo ""
 	  echo "Plex Movie Refreshed."
+   else
+	  echo "Nothing moved - no need to refresh! None!"
+   fi
+   
+fi
+
+if [ -z "$(ls -A $MUS_DIR)" ]; then
+  echo "No Music Content to Upload.. None!"
+else
+  echo ""
+  for (( i = 0 ; i < "${#MUS_ARRAY[@]}"; i++ )); do
+	MUS_MATCH=$(basename "${MUS_ARRAY[$i]}")
+    echo "$MUS_MATCH"
+	if [ -d "/mnt/gdrive/plex_enc/music/$MUS_MATCH" ]; then
+	  echo "$MUS_MATCH already at destination"
+	else
+	  /usr/bin/rclone move --config /home/ndo/.config/rclone/rclone.conf --delete-empty-src-dirs --log-file /opt/rclone_upload_logs/rclone_music_move_$TIME.log --log-level INFO --drive-chunk-size 16M "$MUS_DIR"/"$MUS_MATCH" GdriveEnc:plex_enc/music/"$MUS_MATCH"
+	  echo "${MUS_ARRAY[$i]} moved"
+	  mus_counter=$((mus_counter+1))
+	fi
+  done
+  echo ""
+  if (( mus_counter > 0 )); then
+	  sleep 20
+	  echo "Refreshing Music Library..."
+	  curl http://ndo2.iamnico.xyz:32400/library/sections/7/refresh?X-Plex-Token=UpkkEa7jE1dmneA4orEm >> /dev/null 2>&1
+	  echo ""
+	  echo "Plex Music Refreshed."
    else
 	  echo "Nothing moved - no need to refresh! None!"
    fi
