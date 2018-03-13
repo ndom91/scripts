0a6eb3270a8ee5364f09bdc9c8edea86bb162429
ndom91 <yo@iamnico.xyz>
2018-03-13 17:01:49 +0100

    updated server name for ndo2 	modified:   mounts/sshfs_ndo2.sh

--- mounts/sshfs_ndo2.sh
+++ mounts/sshfs_ndo2.sh
@@ -4,4 +4,4 @@
 # TOO SHORT FOR COMMENTS
 ##########################
 
-sudo /usr/bin/sshfs -p 2022 -o allow_other,default_permissions,uid=1000,gid=1000,IdentityFile=/home/ndo/Documents/Config/ndo2.ppk ndo@ndo2.iamnico.xyz:/ /opt/ndo2_home
+sudo /usr/bin/sshfs -p 2022 -o allow_other,default_permissions,uid=1000,gid=1000,IdentityFile=/home/ndo/Documents/Config/ndo2.ppk ndo@iamnico.xyz:/ /opt/ndo2_home
