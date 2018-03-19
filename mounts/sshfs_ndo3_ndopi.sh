779d76a4f9a88a71e3dacf24f204f76cb9f1d46f
ndo <yo@iamnico.xyz>
2018-03-19 19:59:25 +0100

    removed ndo0 mount 	deleted:    mounts/sshfs_ndo0.sh 	modified:   mounts/sshfs_ndo3_ndopi.sh

--- mounts/sshfs_ndo3_ndopi.sh
+++ mounts/sshfs_ndo3_ndopi.sh
@@ -4,4 +4,4 @@
 # TOO SHORT FOR COMMENTS
 ##########################
 
-/usr/bin/sshfs -o allow_other,default_permissions,uid=1000,gid=1000 ndo@192.168.178.52:/mnt/NDO_Backup /mnt/NDO_Backup
+/usr/bin/sshfs -o allow_other,default_permissions,uid=1000,gid=1000 ndo@192.168.178.52:/ /opt/ndopi_home
