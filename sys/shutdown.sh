f134b1734dc5e36a1924d01bf51f09c7d94c35cc
ndo <yo@iamnico.xyz>
2018-04-28 09:58:29 +0200

    update shutdown.sh

--- sys/shutdown.sh
+++ sys/shutdown.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-shutdown now
+/usr/bin/gxmessage -center -geometry 400x120 -borderless -ontop -fn "monospace 24" "Shutdown in 2 min!"
+
+#/usr/bin/zenity --info --text="Shutdown in 2 Minutes. GET READY FOR WORK!"
+
+/sbin/shutdown
+
