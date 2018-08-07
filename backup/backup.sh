7fb2b18abc97fd64aa8b60aaa5306f9eeb1e2bd6
Nico Domino <ndomino@iamnico.xyz>
2018-08-07 15:31:05 +0200

    fixed up ndo3 backup script

--- backup/backup.sh
+++ backup/backup.sh
@@ -23,6 +23,9 @@ echo "Starting backup..."
 # !!watch excludes - must be customized for each system!!
 ##########################################################
 
+# test taring line
+#tar --exclude=.git -cvpzf $DESDIR/$FILENAME /home/ndo/Documents/scripts >> /dev/null 2>&1
+
 tar --exclude=/swapfile \
 --exclude-backups \
 --exclude-caches-all \
@@ -54,16 +57,55 @@ $DESDIR/$FILENAME $SRCDIR
 # CLEAN UP
 ####################################
 
-echo "Backup complete. Now moving to ndo-pi.."
+echo "Backup complete. Now moving to rclone mega:/ndoX_backup/ndo3"
+echo ""
 
-/usr/bin/rclone copy $DESDIR/$FILENAME mega:/ndoX_backup/ndo3/$FILENAME
+rclone copyto $DESDIR/$FILENAME mega:/ndoX_backup/ndo3_backups/$FILENAME
 
-mv $DESDIR/$FILENAME /opt/ndopi_home/mnt/NDO_Backup/ndo3_backup/$FILENAME
+echo ""
+echo "Rclone move complete. Now moving to open-pi.."
 
-echo "Move complete, sending mail"
+# If open-pi is mounted, continue with copy. If not - mount and then continue
+if [ -d "/opt/ndopi_home/mnt/NDO_Backup" ]; then
+	echo ""
+	echo "open-pi mounted. Continuing.."
 
-./mail_backup.sh
+	cp --no-preserve=mode,ownership $DESDIR/$FILENAME /opt/ndopi_home/mnt/NDO_Backup/ndo3_backup
 
-echo "Mail complete. Script complete. Have a nice day!"
+	# if copy to open-pi was successful delete local copy
+	if [ ! -f /opt/ndopi_home/mnt/NDO_Backup/ndo3_backup/$FILENAME ]; then
+		echo "File not found on open-pi/NDO_Backup, trying again.."
+		cp --no-preserve=mode,ownership $DESDIR/$FILENAME /opt/ndopi_home/mnt/NDO_Backup/ndo3_backup
+		echo "Copy attempt #2 complete"
+
+	else
+		rm $DESDIR/$FILENAME
+	fi
+else
+	echo ""
+	echo "open-pi not mounted. Mounting now.."
+
+	umount /opt/ndopi_home
+	fusermount -u /opt/ndopi_home
+	bash /home/ndo/Documents/scripts/mounts/sshfs_ndo3_ndopi.sh
+
+        cp --no-preserve=mode,ownership $DESDIR/$FILENAME /opt/ndopi_home/mnt/NDO_Backup/ndo3_backup
+
+        # if copy to open-pi was successful delete local copy
+        if [ ! -f /opt/ndopi_home/mnt/NDO_Backup/ndo3_backup/$FILENAME ]; then
+                echo "File not found on open-pi/NDO_Backup, trying again.."
+                cp --no-preserve=mode,ownership $DESDIR/$FILENAME /opt/ndopi_home/mnt/NDO_Backup/ndo$
+                echo "Copy attempt #2 complete"
+        else
+                rm $DESDIR/$FILENAME
+        fi
+fi
+
+echo ""
+echo "Backup complete"
+
+#./mail_backup.sh
+
+#echo "Mail complete. Script complete. Have a nice day!"
 
 
