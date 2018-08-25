7a57d5eae39ea61d5f4193844a1eb33f218e3925
ndo_nt <yo@iamnico.xyz>
2018-08-25 18:17:31 +0200

    updated

--- planner/rtm_planner.sh
+++ planner/rtm_planner.sh
@@ -24,14 +24,16 @@ Subject: on the daily ($CURDATE)
 Content-Type: text/html
 MIME-Version: 1.0
 
-" > planner_$CURDATE.html
+" > /home/ndo/planner/planner_$CURDATE.html
 
-/usr/local/bin/rtm planner due:today OR due:tomorrow | aha --pink >> planner_$CURDATE.html
+/usr/local/bin/rtm planner due:today | aha --pink >> /home/ndo/planner/planner_$CURDATE.html
 
-sed -Ei 's/<pre>/<pre style="color:#fff"><br><span style="font-size:18px;display:block;text-align:center;margin:0 auto;font-weight:bold;">dont forget ninja!<\/span><div style="text-align:center;margin:0 auto;">/g' planner_$CURDATE.html
-sed -Ei 's/style="color:black;"/style=""/g' planner_$CURDATE.html
-sed -Ei 's/pink/#326273/g' planner_$CURDATE.html
-sed -Ei 's/<\/span><\/pre>/<\/div><\/span><\/pre>/g' planner_$CURDATE.html
+sed -Ei 's/<pre>/<pre style="color:#fff"><br><span style="font-size:18px;display:block;text-align:center;margin:0 auto;font-weight:bold;">dont forget ninja!<\/span><div style="text-align:center;margin:0 auto;">/g' /home/ndo/planner/planner_$CURDATE.html
+sed -Ei 's/style="color:black;"/style=""/g' /home/ndo/planner/planner_$CURDATE.html
+sed -Ei 's/pink/#326273/g' /home/ndo/planner/planner_$CURDATE.html
+sed -Ei 's/<\/span><\/pre>/<\/div><\/span><\/pre>/g' /home/ndo/planner/planner_$CURDATE.html
 
 
-/usr/sbin/sendmail -t < planner_$CURDATE.html
+/usr/sbin/sendmail -t < /home/ndo/planner/planner_$CURDATE.html
+
+find '/home/ndo/planner/' -mtime +6 -type f -delete
