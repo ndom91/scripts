f941b06f6b7819c3939bff3f8e73cafd51b63952
Nico [open-pi] <yo@iamnico.xyz>
2018-09-02 00:23:39 +0200

    updated to pi-hole teleporter backup

--- backup/backup_open-pi_daily.sh
+++ backup/backup_open-pi_daily.sh
@@ -12,6 +12,7 @@
 # VARIABLES
 ###################
 
+DATE=$(date "+%d%m%y")
 TIME=$(date +%b-%d-%y)
 FILENAME="backup-configs-openpi-$TIME.tar.gz"
 FILENAME2="backup-openhab2-$TIME.zip"
@@ -34,12 +35,15 @@ echo "Starting backup..."
 # TAR BALL ALL THE DIRS!
 ##########################################################
 
-/usr/share/openhab2/runtime/bin/backup $DESDIR/$FILENAME2
+mkdir $DESDIR/$DATE
+/usr/share/openhab2/runtime/bin/backup $DESDIR/$DATE/$FILENAME2
+cd $DESDIR/$DATE
+/usr/local/bin/pihole -a -t
 
 tar \
 -cvpzf \
 $DESDIR/$FILENAME \
-/etc/pihole \ >> /dev/null 2>&1
+$DESDIR/$DATE >> /dev/null 2>&1
 
 ####################################
 # CLEAN UP
@@ -52,10 +56,8 @@ echo "[*] Now moving to /mnt/NDO_Backup/pi_backups/open-pi"
 echo "    and mega:/ndoX_backup/open-pi/"
 
 sudo cp $DESDIR/$FILENAME /mnt/NDO_Backup/pi_backups/open-pi
-sudo cp $DESDIR/$FILENAME2 /mnt/NDO_Backup/pi_backups/open-pi
 
 /usr/bin/rclone move --config /home/pi/.config/rclone/rclone.conf --log-file /home/pi/rclonelogs/backup-$TIME.log $DESDIR/$FILENAME mega:ndoX_backup/open-pi >> /dev/null
-/usr/bin/rclone move --config /home/pi/.config/rclone/rclone.conf --log-file /home/pi/rclonelogs/backup-$TIME.log $DESDIR/$FILENAME2 mega:ndoX_backup/open-pi >> /dev/null
 
 echo ""
 
@@ -71,6 +73,16 @@ echo "$ rm "$DESDIR"/"$FILENAME
 
 find /mnt/NDO_Backup/pi_backups/open-pi -type f -mtime +10 -delete
 
+if [ -f /mnt/NDO_Backup/pi_backups/open-pi/$FILENAME ]; then
+	rm -r $DESDIR/$DATE
+	echo ""
+	echo "[*] File succesfully transfered. Local '$DATE' directory deleted."
+else
+	echo ""
+	echo "[*] File UNSUCCESSFULLY transfered to NDO_Backup - manually check!"
+	echo "[*] NOT deleting $DESDIR/$DATE !!! "
+fi
+
 echo ""
 
 echo "[*] Clean up complete"
@@ -78,16 +90,13 @@ echo "[*] Clean up complete"
 echo ""
 
 FILESIZE=$(/usr/bin/rclone ls mega:/ndoX_backup/open-pi/backup-configs-openpi-$TIME.tar.gz | awk '{print $1}')
-FILESIZE2=$(/usr/bin/rclone ls mega:/ndoX_backup/open-pi/backup-openhab2-$TIME.zip | awk '{print $1}')
 FILESIZEa=$(/usr/bin/bc -l <<< "scale=2; $FILESIZE / 1048576")
-FILESIZE2a=$(/usr/bin/bc -l <<< "scale=2; $FILESIZE / 1048576")
 
 echo "[*] Backup complete!"
 echo ""
 echo "########################################"
 echo "#"
-echo "#     pi-hole size: "$FILESIZEa "mb"
-echo "#     opehab2 size: "$FILESIZE2a "mb"
+echo "#        size: "$FILESIZEa "mb"
 echo "#"
 echo "#        "$(date)
 echo "#"
