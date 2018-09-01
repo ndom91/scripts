3b5ace876cfa1680ee030aa6ba0011660dfbe841
Nico [open-pi] <yo@iamnico.xyz>
2018-09-01 23:59:35 +0200

    added official openhab2 backup

--- backup/backup_open-pi_daily.sh
+++ backup/backup_open-pi_daily.sh
@@ -14,7 +14,7 @@
 
 TIME=$(date +%b-%d-%y)
 FILENAME="backup-configs-openpi-$TIME.tar.gz"
-SRCDIR="/"
+FILENAME2="backup-openhab2-$TIME.zip"
 DESDIR=/home/pi/Backups
 
 echo "########################################"
@@ -34,13 +34,12 @@ echo "Starting backup..."
 # TAR BALL ALL THE DIRS!
 ##########################################################
 
+/usr/share/openhab2/runtime/bin/backup $DESDIR/$FILENAME2
+
 tar \
 -cvpzf \
 $DESDIR/$FILENAME \
-/etc/pihole \
-/var/lib/openhab2 \
-/usr/share/openhab2 \
-/etc/openhab2 \ >> /dev/null 2>&1
+/etc/pihole \ >> /dev/null 2>&1
 
 ####################################
 # CLEAN UP
@@ -53,8 +52,10 @@ echo "[*] Now moving to /mnt/NDO_Backup/pi_backups/open-pi"
 echo "    and mega:/ndoX_backup/open-pi/"
 
 sudo cp $DESDIR/$FILENAME /mnt/NDO_Backup/pi_backups/open-pi
+sudo cp $DESDIR/$FILENAME2 /mnt/NDO_Backup/pi_backups/open-pi
 
 /usr/bin/rclone move --config /home/pi/.config/rclone/rclone.conf --log-file /home/pi/rclonelogs/backup-$TIME.log $DESDIR/$FILENAME mega:ndoX_backup/open-pi >> /dev/null
+/usr/bin/rclone move --config /home/pi/.config/rclone/rclone.conf --log-file /home/pi/rclonelogs/backup-$TIME.log $DESDIR/$FILENAME2 mega:ndoX_backup/open-pi >> /dev/null
 
 echo ""
 
@@ -77,13 +78,16 @@ echo "[*] Clean up complete"
 echo ""
 
 FILESIZE=$(/usr/bin/rclone ls mega:/ndoX_backup/open-pi/backup-configs-openpi-$TIME.tar.gz | awk '{print $1}')
-FILESIZE2=$(/usr/bin/bc -l <<< "scale=2; $FILESIZE / 1048576")
+FILESIZE2=$(/usr/bin/rclone ls mega:/ndoX_backup/open-pi/backup-openhab2-$TIME.zip | awk '{print $1}')
+FILESIZEa=$(/usr/bin/bc -l <<< "scale=2; $FILESIZE / 1048576")
+FILESIZE2a=$(/usr/bin/bc -l <<< "scale=2; $FILESIZE / 1048576")
 
 echo "[*] Backup complete!"
 echo ""
 echo "########################################"
 echo "#"
-echo "#        Size: "$FILESIZE2 "mb"
+echo "#     pi-hole size: "$FILESIZEa "mb"
+echo "#     opehab2 size: "$FILESIZE2a "mb"
 echo "#"
 echo "#        "$(date)
 echo "#"
