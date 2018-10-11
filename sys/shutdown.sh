ca223ca021fa6e876b6dac555cbda21f33c21f0e
Nico Domino <ndomino@iamnico.xyz>
2018-10-11 20:39:51 +0200

    update shutdown.sh FINALLY got notify-alert to work

--- sys/shutdown.sh
+++ sys/shutdown.sh
@@ -2,19 +2,12 @@
 
 # TIMER
 
-curr=$(date +"%H:%M:%S")
 date2=$(date -d "($cur) +2minutes" +%H:%M:%S)
 
-# POPUP Message
-
-export DISPLAY=:0
-
-DBUS=$(pgrep -ou ndo xfce)
-DBUS="$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$DBUS/environ | sed 's/DBUS_SESSION_BUS_ADDRESS=//')"
-DBUS_SESSION_BUS_ADDRESS="$DBUS" /usr/bin/notify-send  -i computer 'Shutdown Requested' 'Computer will shutdown at '$date2
-
-# OPTIONAL EXTRA LINE:
-#\nIt is currently '$(date "+%H:%M:%S")
+user=ndo
+export HOME=/home/$user
+source "$HOME/.dbus/session-bus/*-0"
+/usr/bin/notify-send 'Shutdown!' 'ndo3 will shutdown in T-00:02:00' --icon=computer
 
 # SHUTDOWN
 
